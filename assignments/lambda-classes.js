77d7fda6f19a3d3667b8156ce267f1583c023ada
myasia barbee <myasiabarbee@icloud.com>
2019-04-05 19:50:37 -0400

    student.classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -44,3 +44,37 @@ class Person{
     }
 
 }
+
+ //Student
+/*  Now we need some students!
+ Student uses the same attributes that have been set up by Person
+ Student has the following unique props:
+   `previousBackground` i.e. what the Student used to do before Lambda School
+   `className` i.e. CS132
+   `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+ Student has the following methods:
+   `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+   `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+   `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+ */
+
+class Student extends Person {
+    constructor (studentinfo){
+        super(studentinfo);
+        this.previousBackground = studentinfo.previousBackground;
+        this.className = studentinfo.className;
+        this.favSubjects = studentinfo.favSubjects;
+    }
+
+    listsSubjects() {
+        console.log(...this.favSubjects);
+    }
+
+    PRAssignment(student, subject){
+        console.log(`${student.name} has submitted a PR for ${subject}`)
+    }
+
+    sprintChallenge(student, subject){
+        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    }
+}
